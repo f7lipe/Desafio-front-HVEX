6c48d29c4118483a2a24dd7b0b711a0e62d07153
= <f7lipe@me.com>
2023-03-23 22:27:59 -0300

    Update GlobalStyles with project's specs

--- src/app/styles/global/GlobalStyle.tsx
+++ src/app/styles/global/GlobalStyle.tsx
@@ -7,10 +7,42 @@ const GlobalStyle = createGlobalStyle`
     box-sizing: border-box;
     background-color: white;
     }
+
+    h1{
+        font-size: 48px;
+    }
+
+    h2{
+        font-size: 32px;
+    }
+
+    h3{
+        font-size: 24px;
+    }
+
+    h4{
+        font-size: 20px;
+    }
+
+    h5{
+        font-size: 16px;
+    }
+
+    h1, h2, h3 {
+        font-weight: 700;
+    }
+
+    h4, h5 {
+        font-weight: 400;
+    }
+
 `
 const theme = {
     colors: {
-        primary: '#0070f3',
+        primary: '#00B6DE',
+        secondary: '#036FC0',
+        tertiary: '#1C2C44',
+        quaternary: '#1B2033',
     },
 }
 
