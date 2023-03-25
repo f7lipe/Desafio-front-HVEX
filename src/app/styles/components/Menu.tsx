c31b834288f099f51334fd8d41ef2c1aa53d03b7
= <f7lipe@me.com>
2023-03-25 00:32:28 -0300

    feat: Menu and MenuItems for building Navbar

---
+++ src/app/styles/components/Menu.tsx
@@ -0,0 +1,47 @@
+import styled from "styled-components"
+
+interface Props {
+  isOpen?: boolean
+}
+
+const MobileMenu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  align-items: space-between;
+  list-style: none;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  height: 70px;
+  transition: height 0.5s ease-in-out;
+  
+    ${(props: Props) => props.isOpen && `
+    height: 100vh;
+  `}
+
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`
+
+const MenuItem = styled.li`
+  margin: 10px 0px;
+
+  @media screen and (min-width: 768px) {
+    margin: 0px 20px;
+  }
+`
+
+const DesktopMenu = styled.ul`
+  display: flex;
+  width: 100%;
+  list-style: none;
+  align-items: center;
+  justify-content: space-between;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
+`
+
+export { MobileMenu, MenuItem, DesktopMenu }
