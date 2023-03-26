a0a272eaa543782f827983022938b49ae749ff45
= <f7lipe@me.com>
2023-03-26 16:00:04 -0300

    update Group components adding backGroundColor and padding props

--- src/app/styles/components/Group.tsx
+++ src/app/styles/components/Group.tsx
@@ -7,7 +7,9 @@ interface Props {
     width?: string
     height?: string
     margin?: string
+    padding?: string
     titleColor?: string
+    backgroundColor?: string
 }
 
 const Group = styled.div`
@@ -16,8 +18,10 @@ const Group = styled.div`
     width: ${(props: Props) => props.width || '100%'};
     height: ${(props: Props) => props.height || '100%'};
     margin: ${(props: Props) => props.margin || '0'};
+    padding: ${(props: Props) => props.padding || '0'};
     align-items: ${(props: Props) => props.align || 'stretch'};
     justify-content: ${(props: Props) => props.justify || 'flex-start'};
+    background-color: ${(props: Props) => props.backgroundColor || 'transparent'};
 
     @media (min-width: 768px) {
         flex-direction: ${(props: Props) => props.direction || 'row'};
