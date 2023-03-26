923fe2caa11fdcfa652294a75c6758e6a974fe2f
= <f7lipe@me.com>
2023-03-25 21:49:18 -0300

    Update Group component with flex-direction and media query

--- src/app/styles/components/Group.tsx
+++ src/app/styles/components/Group.tsx
@@ -6,14 +6,22 @@ interface Props {
     align?: 'stretch' | 'flex-start' | 'flex-end' | 'center' | 'baseline'
     width?: string
     height?: string
+    margin?: string
+    titleColor?: string
 }
 
 const Group = styled.div`
     display: flex;
+    flex-direction: ${(props: Props) => props.direction || 'column'};
     width: ${(props: Props) => props.width || '100%'};
     height: ${(props: Props) => props.height || '100%'};
+    margin: ${(props: Props) => props.margin || '0'};
     align-items: ${(props: Props) => props.align || 'stretch'};
     justify-content: ${(props: Props) => props.justify || 'flex-start'};
+
+    @media (min-width: 768px) {
+        flex-direction: ${(props: Props) => props.direction || 'row'};
+    }
 `
 
 export { Group }
