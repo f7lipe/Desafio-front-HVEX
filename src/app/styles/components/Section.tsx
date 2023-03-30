37dd8db6d9b615873e95cf5c9c7e914ff3597118
= <f7lipe@me.com>
2023-03-29 22:47:03 -0300

    update Section Component with splitted background props

--- src/app/styles/components/Section.tsx
+++ src/app/styles/components/Section.tsx
@@ -4,6 +4,7 @@ interface Props{
     backgroundColor?: string;
     color?: string;
     margin?: string;
+    splitBackground?: boolean;
     align?: 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch';
     justify?: 'flex-start' | 'flex-end' | 'center' | 'space-between' | 'space-around' | 'space-evenly' | 'stretch';
 }
@@ -17,9 +18,10 @@ export const Section = styled.section`
     padding: 10px 10px;
     width: 100%;
     height: 100%;
-
+    
     @media screen and (min-width: 768px) {
         flex-direction: row;
         padding: 10px 40px;
+        background-image: ${(props: Props) => props.splitBackground ? 'linear-gradient(90deg, #000 80%, #fff 20%)' : 'none'};
     }
 `
